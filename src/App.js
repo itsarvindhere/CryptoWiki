3226673bb637870376cf283889542eace371aa32
Arvind <ranaarvind156@gmail.com>
2022-05-01 11:56:01 +0530

    Add missing link in the footer

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ const App = () => {
       </Typography.Title>
       <Space>
         <Link to="/">Home</Link>
+        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
         <Link to="/news">News</Link>
       </Space>
       </footer>
